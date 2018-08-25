40b4d2c54ddd3f987c54463f318aed5aa58c736f
Andrew <andrescastillo.r@hotmail.com>
2018-08-25 00:55:45 -0400

    clean key strings

--- components/SideBar.js
+++ components/SideBar.js
@@ -16,6 +16,21 @@ const docs = [
 ];
 
 export default class SideBar extends React.Component {
+  cleanString(path) {
+    let path_array = path.split('');
+    let i = 0;
+    while(path_array[i]) {
+      if(path_array[i] == '/'){
+        path_array.splice(0, i+1); //remove the '/' and everything before it
+        i = 0; // start from the new beginning
+      }else{
+        i++;
+      }
+    }
+    return path_array.join('');
+
+  }
+
   renderList(docs, navigation) {
     let list = [];
     docs.forEach((doc) => {
@@ -29,12 +44,13 @@ export default class SideBar extends React.Component {
       );
       if(doc.doc_data){
         for (let key in doc.doc_data) {
+          let clean_key = this.cleanString(key);
           list.push(
             <ListItem 
               button
-              onPress={() => navigation.navigate((doc.isDoc)? 'DocView' : 'Home', {title: doc.name, subtitle: key, doc_html: doc.doc_data[key]})}
+              onPress={() => navigation.navigate((doc.isDoc)? 'DocView' : 'Home', {title: doc.name, subtitle: clean_key, doc_html: doc.doc_data[key]})}
             >
-              <Text>{key}</Text>
+              <Text>{clean_key}</Text>
             </ListItem>
           );
         }
--- views/DocView.js
+++ views/DocView.js
@@ -3,12 +3,6 @@ import { WebView, ScrollView, Dimensions } from 'react-native';
 import { Container, Header, Left, Right, Button, Body, Title, Subtitle, Content, Text, Icon } from 'native-base';
 
 export default class DocView extends React.Component {
-	/*
-		In the future the label could come from a prop, the list that contains the currently downloaded docsets.
-
-		Also in the future I should find another way of adding the drawer labels, in accordance to the number of docsets that are currently downloaded
-		See https://github.com/react-navigation/react-navigation/issues/1210#issuecomment-297450371 for using this DocView for any docset
-	*/
 	render() {
 		return (
 			<Container>
@@ -22,7 +16,7 @@ export default class DocView extends React.Component {
             <Title>{ this.props.navigation.getParam('title', 'Undefined title') }</Title>
             <Subtitle>{ this.props.navigation.getParam('subtitle', 'Undefined subtitle') }</Subtitle>
           </Body>
-          <Right />
+          <Right/>
         </Header>
 				<WebView 
 					source={{html: this.props.navigation.getParam('doc_html', '<h1>Undefined</h1>')}}
