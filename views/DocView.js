710532bd15dd6a730993ef17ba0d209af7d4c5e0
Andrew <andrescastillo.r@hotmail.com>
2018-08-27 13:54:39 -0400

    add different component to present html, looks weird though, it may need more styles

--- views/DocView.js
+++ views/DocView.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { WebView, ScrollView, Dimensions } from 'react-native';
-import { Container, Header, Left, Right, Button, Body, Title, Subtitle, Content, Text, Icon } from 'native-base';
+import HtmlView from 'react-native-render-html';
+import { Container, Header, Left, Right, Button, Body, Title, Subtitle, Icon } from 'native-base';
 
 export default class DocView extends React.Component {
 	render() {
@@ -18,10 +19,17 @@ export default class DocView extends React.Component {
           </Body>
           <Right/>
         </Header>
-				<WebView 
+        <ScrollView style={{ flex: 1}}>
+        	<HtmlView
+        		html={this.props.navigation.getParam('doc_html', '<h1>Undefined</h1>')}
+        		imagesMaxWidth={Dimensions.get('window').width}
+        	/>
+        </ScrollView>
+				{/*<WebView
+					style={styles.WebViewStyle}
 					source={{html: this.props.navigation.getParam('doc_html', '<h1>Undefined</h1>')}}
 					originWhiteList={["*"]}
-				/>
+				/>*/}
 			</Container>
 		);
 	}
